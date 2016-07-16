997e2383fbce8935f8f1a18f065577c680d8fe0b
Payton Swick <payton@foolord.com>
2016-07-16 15:12:59 -0400

    Make sure WP_Error does not exist before Globals are included

--- tests/QuasiWPTest.php
+++ tests/QuasiWPTest.php
@@ -5,6 +5,7 @@ use PHPUnit\Framework\TestCase;
 class QuasiWPTest extends TestCase {
 
 	public function test_mock_wp_defines_wp_error() {
+		$this->assertFalse( class_exists( 'WP_Error' ) );
 		\QuasiWP\mock_wp();
 		$this->assertTrue( class_exists( 'WP_Error' ) );
 	}
